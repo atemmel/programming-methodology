df84cbcb6d2a35f103b29e432e4a77d7b8aff703
atemmel <temmeladam@gmail.com>
2018-12-04 09:34:34 +0100

    Changed stuff in lab3

---
+++ lab3/main.cpp
@@ -0,0 +1,14 @@
+#include "p_queue.h"
+
+#include <iostream>
+#include <string>
+#include <random>
+
+
+int main()
+{
+	p_queue<std::string> queue;
+
+
+	return 0;
+}
--- lab3/p_queue.h
+++ lab3/p_queue.h
@@ -1,35 +1,32 @@
 #ifndef P_QUEUE_H
 #define P_QUEUE_H
 
+#include <functional>
 #include <vector>
 
-
-struct compare_less
-{
-	bool operator()(int lhs, int rhs) const
-	{
-		return lhs < rhs;
-	}
-};
-
-template<typename T, comparison = compare_less>
+template<typename T, typename comparison = std::less<>>
 class p_queue
 {
 public:
 	T pop()
 	{
-
+		T value = m_elements.front().value;
+		m_elements.erase(m_elements.begin());
+		return value;
 	}
 
 	void push(const T & value, int priority = 0)
 	{
 		auto it = m_elements.begin();
+		comparison comp;
 
 		while(it != m_elements.end())
 		{
-			if(
+			if(comp(it->priority, priority)) break;
 			++it;
 		}
+
+		m_elements.insert(it, value);
 	}
 
 	size_t size() const
